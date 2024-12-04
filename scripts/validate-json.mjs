8d49b907f5c42db913af94ba43e1d33bc282892f
mattbrailsford <me@mattbrailsford.com>
2024-12-04 07:30:15 +0000

    Try catch error

--- scripts/validate-json.mjs
+++ scripts/validate-json.mjs
@@ -8,25 +8,33 @@ const type = args[0];
 
 const ajv = new Ajv({ strict: false });
 
-// Define custom formats
-ajv.addFormat('url_safe', /^[a-z0-9\-\_]*$/gi)
+try
+{
+    // Define custom formats
+    ajv.addFormat('url_safe', /^[a-z0-9\-\_]*$/gi)
 
-// Load json + schema
-const jsonRaw = await fs.readFile(path.resolve(`./data/${type}.json`), "utf8");
-const schemaRaw = await fs.readFile(path.resolve(`./schemas/${type}.schema.json`), "utf8");
+    // Load json + schema
+    const jsonRaw = await fs.readFile(path.resolve(`./data/${type}.json`), "utf8");
+    const schemaRaw = await fs.readFile(path.resolve(`./schemas/${type}.schema.json`), "utf8");
 
-const json = JSON.parse(jsonRaw);
-const schema = JSON.parse(schemaRaw);
+    const json = JSON.parse(jsonRaw);
+    const schema = JSON.parse(schemaRaw);
 
-// Validate the json against the schema
-const validate = ajv.compile(schema);
-const isValid = validate(json);
+    // Validate the json against the schema
+    const validate = ajv.compile(schema);
+    const isValid = validate(json);
 
-// Output the message
-if (!isValid) {
-    console.log(`${type}.json is invalid`);
-    console.error(prettify(validate, { data: json }));
+    // Output the message
+    if (!isValid) {
+        console.log(`${type}.json is invalid`);
+        console.error(prettify(validate, { data: json }));
+        process.exit(1);
+    } else {
+        // You don't need to print anything when it passes.
+    }
+} 
+catch (err) 
+{
+    console.error(err.message);
     process.exit(1);
-} else {
-    // You don't need to print anything when it passes.
 }
